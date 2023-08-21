5944bf6396728554a6ed2ee9d8a6180dc72132aa
TainaRegina <tainareginasoares@hotmail.com>
2023-08-21 19:28:22 -0300

    tela detalhes de produtos ok

--- app/src/main/java/com/example/pizaria_stackmobile/adapter/ProductAdapter.kt
+++ app/src/main/java/com/example/pizaria_stackmobile/adapter/ProductAdapter.kt
@@ -10,9 +10,11 @@ getItemCount(): a RecyclerView chama esse método para ver o tamanho do conjunto
  */
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.pizaria_stackmobile.ProductDetails
 import com.example.pizaria_stackmobile.databinding.ProductItemBinding
 import com.example.pizaria_stackmobile.model.Product
 
@@ -31,12 +33,22 @@ class ProductAdapter(private val context: Context, private val productList: Muta
         holder.imgProduct.setBackgroundResource(productList[position].imgProduct)
         holder.name.text = productList[position].name
         holder.price.text = productList[position].price
+
+        //event Click
+        holder.btnAdd.setOnClickListener{
+            val intent = Intent(context, ProductDetails::class.java)
+            intent.putExtra("imgProduct", productList[position].imgProduct)
+            intent.putExtra("name", productList[position].name)
+            intent.putExtra("price", productList[position].price)
+            context.startActivity(intent)
+        }
     }
 
     inner class ProductViewHolder(binding: ProductItemBinding): RecyclerView.ViewHolder(binding.root){
         val imgProduct = binding.imgProduct
         val name = binding.txtName
         val price = binding.txtPrice
+        val btnAdd = binding.btnAdd
     }
 
 }
