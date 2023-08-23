cc4ed1d584eca75a700fa5455af63103d9be1c7a
TainaRegina <tainareginasoares@hotmail.com>
2023-08-23 19:51:30 -0300

    layout pagamento ok

--- app/src/main/java/com/example/pizaria_stackmobile/Payment.kt
+++ app/src/main/java/com/example/pizaria_stackmobile/Payment.kt
@@ -1,8 +1,11 @@
 package com.example.pizaria_stackmobile
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pizaria_stackmobile.databinding.ActivityPaymentBinding
+import java.text.DecimalFormat
 
 class Payment : AppCompatActivity() {
 
@@ -12,5 +15,16 @@ class Payment : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPaymentBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        window.statusBarColor = Color.parseColor("#E0E0E0")
+
+        val name = intent.extras!!.getString("name")
+        val amount = intent.extras!!.getString("amount")
+        val total = intent.extras!!.getString("total")
+        val saucesAndDrinks = intent.extras!!.getString("saucesAndDrinks")
+        val decimalFormat = DecimalFormat.getCurrencyInstance()
+
+
+
     }
 }
